d6219d86ec5f52c09fb8bdfed8b62837917edb8f
Shervin <sherviniv@gmail.com>
2021-07-09 16:00:17 +0430

    Initial commit

---
+++ src/Micro-Blog/Services/Post/Post.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        public string LastModifiedBy { get; set; }
+    }
+}
---
+++ src/Micro-Blog/Services/Post/Post.Domain/Entities/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿using Post.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Domain.Entities
+{
+    public class BlogPost : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Img { get; set; }
+        public bool Published { get; set; }
+    }
+}
