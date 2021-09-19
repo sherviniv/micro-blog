1088b89c21a091553db61b8f427122929aa47ce1
Shervin <sherviniv@gmail.com>
2021-09-19 13:26:22 +0430

    Consuming blog event in Subscription service

---
+++ src/Micro-Blog/Services/Subscription/Subscription.Application/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using EventBus.Common.Constants;
+using EventBus.Common.Extensions;
+using EventBus.Common.Models;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Subscription.Application.EventBusConsumers.Blog;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Subscription.Application
+{
+    public static class DependencyContainer
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.UseEventBus(new(configuration["EventBusSettings:HostAddress"]), new List<ConsumerDetail>()
+            {
+                new ConsumerDetail() 
+                {
+                     QueueName= EventBusConstants.BlogPublishedQueue,
+                     Consumers = new List<System.Type>{ typeof(BlogPublishedConsumer) } 
+                }
+            });
+        }
+    }
+}
---
+++ src/Micro-Blog/Services/Subscription/Subscription.Application/EventBusConsumers/Blog/BlogPublishedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using EventBus.Common.Abstractions;
+using EventBus.Events.Events;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Subscription.Application.EventBusConsumers.Blog
+{
+    public class BlogPublishedConsumer : IEventConsumer<BlogPublishedEvent>
+    {
+        private readonly ILogger<BlogPublishedConsumer> _logger;
+
+        public BlogPublishedConsumer(ILogger<BlogPublishedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<BlogPublishedEvent> context)
+        {
+            _logger.LogInformation("BlogPublishedConsumer consumed successfully. ", context.Message.PublisherUserId);
+        }
+    }
+}
